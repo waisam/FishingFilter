e19faebcbfc211c0a1c5c95e335fd0370dc1b785
waisam <waisam@live.com>
2020-04-14 09:37:19 +0800

    添加钓鱼产品数据库

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
-### Fishingfilter ###
+## Fishingfilter
 
 协助清理钓鱼垃圾
 
-#### 1.选择以下任一种方式安装
+### 1.选择以下任一种方式安装
 
 * 使用git拉取到本地 Interface/Addons 目录
 * 下载zip压缩包, 解压到本地 Interface/Addons 目录, 并修改目录名为: FishingFilter
 
-#### 2.如何使用
+### 2.如何使用
 
-为钓鱼技能做一个宏, 并且在/cast 钓鱼之前添加/run filterTrashes()
+为钓鱼技能做一个宏, 并添加指令 /run filterTrashes()
+
+### 3.配置产品
+
+在Scripts\data.lua文件中, 搜索物品名, 修改该行的delete值
--- core.lua
+++ core.lua
@@ -1,29 +1,13 @@
--- 米港钓鱼垃圾
-local menethilTrashes={
-	[6361]=true,[6359]=true
-}
-
--- 塔纳利斯钓鱼垃圾
-local tanarisTrashes={
-	[13876]=true,[13877]=true,[13754]=true,
-	[4603]=true,[6362]=true,[13756]=true,
-	[6359]=true,[13875]=true,
-	[7974]=true,[5498]=true,[5500]=true,[7971]=true
-}
-
--- 艾萨拉钓鱼删除垃圾
-local azsharaTrashes={
-	[13893]=true,[13888]=true,[4603]=true,[13756]=true,
-	[6359]=true,[13758]=true,[8365]=true,
-	[7974]=true,[5498]=true,[5500]=true,[7971]=true
-}
+local name, _FishingFilter = ...;
+local fishes = _FishingFilter.fishes;
+local trashes = _FishingFilter.trashes;
 
 -- 删除函数
 function filterTrashes( ... )
 	for b=0,4,1 do 
 		for s =1,32,1 do 
 			local id=select(10, GetContainerItemInfo(b,s)) 
-			if menethilTrashes[id] or tanarisTrashes[id] or azsharaTrashes[id] then 
+			if fishes[id].delete or trashes[id].delete then 
 				PickupContainerItem(b,s)
 				DeleteCursorItem() 
 			end 
